bf3e2e04bb4650656b36c60c1ed86922f1be3779
chen shanmugarajah <chenshanmugarajah@gmail.com>
2020-07-26 18:49:15 +0100

    Initial commit

---
+++ Chen-Blog-App/Controller/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Database;
+
+namespace Controller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

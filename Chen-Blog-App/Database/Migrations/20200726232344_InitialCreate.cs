6832699db0f2f6064d5c992439c1c7b088580108
chen shanmugarajah <chenshanmugarajah@gmail.com>
2020-07-27 00:27:43 +0100

    created ef models to be migrated

---
+++ Chen-Blog-App/Database/Migrations/20200726232344_InitialCreate.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Model.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Passowrd = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Threads",
+                columns: table => new
+                {
+                    ThreadId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(nullable: true),
+                    Owner = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Threads", x => x.ThreadId);
+                    table.ForeignKey(
+                        name: "FK_Threads_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PostTitle = table.Column<string>(nullable: true),
+                    PostContent = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    Likes = table.Column<int>(nullable: false),
+                    UserId = table.Column<int>(nullable: false),
+                    ThreadId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                    table.ForeignKey(
+                        name: "FK_Posts_Threads_ThreadId",
+                        column: x => x.ThreadId,
+                        principalTable: "Threads",
+                        principalColumn: "ThreadId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Posts_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    CommentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PostComment = table.Column<string>(nullable: true),
+                    PostId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.CommentId);
+                    table.ForeignKey(
+                        name: "FK_Comments_Posts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Posts",
+                        principalColumn: "PostId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_PostId",
+                table: "Comments",
+                column: "PostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_ThreadId",
+                table: "Posts",
+                column: "ThreadId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_UserId",
+                table: "Posts",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Threads_UserId",
+                table: "Threads",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Threads");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- Chen-Blog-App/Database/Model.cs
+++ Chen-Blog-App/Database/Model.cs
@@ -1,11 +1,68 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Database
 {
-    public class Model
+    public class BloggingContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Thread> Threads { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=Chen-Blog-App;");
+    }
+
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Passowrd { get; set; }
+        public List<Post> Posts { get; } = new List<Post>();
+        public List<Thread> Threads { get; } = new List<Thread>();
+
+    }
+
+    public class Thread
+    {
+        public int ThreadId { get; set; }
+        public string Description { get; set; }
+        public string Owner { get; set; }
+        public List<Post> Posts { get; } = new List<Post>();
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string PostTitle { get; set; }
+        public string PostContent { get; set; }
+        public string Author { get; set; }
+        public int Likes { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int ThreadId { get; set; }
+        public Thread Thread { get; set; }
+
+        public List<Comment> Comments { get; } = new List<Comment>();
+
+    }
+
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string PostComment { get; set; }
+
+        public int PostId { get; set; }
+        public Post Post { get; set; }
     }
 }
